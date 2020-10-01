7d3037c8324a75eab81d108802ae6d0ad74af401
ifeniprun16 <ifeniprun16@gmail.com>
2020-10-01 19:13:18 +0700

    fix js asset  in view

--- resources/views/open_minds/success.blade.php
+++ resources/views/open_minds/success.blade.php
@@ -34,7 +34,7 @@
 <script crossorigin="anonymous" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
         src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 
-<script src="{{'../assets/js/bootstrap.bundle.js'}}">
+<script src="{{asset('js/bootstrap.bundle.js')}}">
 
 </script>
 </body>
