42677aaac43c807dc9e71eda0d86b9331240a358
Ifen Faridian <ifeniprun16@gmail.com>
2021-06-27 22:26:43 +0700

    up

--- resources/views/open_minds/index.blade.php
+++ resources/views/open_minds/index.blade.php
@@ -5,10 +5,10 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>EDE Laboratory</title>
     <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
-    <link href="{{asset('css/custom.css')}}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Source+Sans+Pro&display=swap"
-          rel="stylesheet">
+    rel="stylesheet">
+    <link href="{{asset('css/custom.css')}}" rel="stylesheet">
 </head>
 <body class="form-body">
 <div class="container">
